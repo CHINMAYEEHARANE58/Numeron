ac5a3627458436ea73ddf3d1f0c483a8505e6938
CHINMAYEEHARANE58 <chinmayee.harane@kalvium.community>
2023-11-03 13:38:11 +0530

    done

--- score.js
+++ score.js
@@ -1,1 +1,15 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+const scoreBox = document.getElementById("score-board");
+
+// will get the score from our vault
+let passcode = localStorage.getItem("squad56");
+console.log(passcode)
+
+scoreBox.innerHTML = passcode;
+
+const playAgain = document.getElementById("play-again-button")
+
+playAgain.onclick = () =>{
+    location.href = " ./game.html"
+}
